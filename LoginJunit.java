0d6000f10b0ca943a26c549c09cf06cd167f47fd
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-29 22:44:14 +0530

    [Jitendra] Added: Validate last name with junit testing

--- LoginJunit.java
+++ LoginJunit.java
@@ -20,7 +20,7 @@ public class LoginJunit {
     @Test
     public void first_name_validation_if_return_false()
     {
-        String firstname="Jitendra";
+        String firstname="jitendra";
         boolean result= user.FirstNameValidation(firstname);
         Assert.assertEquals(false,result);
 
@@ -28,8 +28,32 @@ public class LoginJunit {
     @Test
     public void if_No_of_char_less_than_three_return_false()
     {
-        String firstname="Jp";
+        String firstname="Ji";
         boolean result= user.FirstNameValidation(firstname);
+        Assert.assertEquals(false,result);
+
+    }
+    @Test
+    public void last_name_validation_if_return_true()
+    {
+        String lastname="Patel";
+        boolean result= user.LastNameValidatation(lastname);
+        Assert.assertEquals(true,result);
+
+    }
+    @Test
+    public void last_Name_validation_if_return_false()
+    {
+        String lastname="patel";
+        boolean result= user.LastNameValidatation(lastname);
+        Assert.assertEquals(false,result);
+
+    }
+    @Test
+    public void if_no_of_char_less_than_three_return_false()
+    {
+        String lastname="Pa";
+        boolean result= user.LastNameValidatation(lastname);
         Assert.assertEquals(false,result);
 
     }
--- LoginMain.java
+++ LoginMain.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.JunitTest;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginMain {
-
     public static boolean FirstNameValidation(String firstname)
     {
         Pattern pattern = Pattern.compile("^[A-Z]{1}(?=.*[a-z]).{2,}$");
@@ -18,6 +18,24 @@ public class LoginMain {
         if(!found)
         {
             System.out.println("Invalid First Name");
+
+        }
+        return found;
+
+    }
+    public static boolean LastNameValidatation(String name)
+    {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}(?=.*[a-z]).{2,}$");
+        Matcher matcher =  pattern.matcher(name);
+        boolean found = false;
+        while (matcher.find())
+        {
+            System.out.println("Valid Last Name");
+            found = true;
+        }
+        if(!found)
+        {
+            System.out.println("Invalid Last name");
 
         }
         return found;
